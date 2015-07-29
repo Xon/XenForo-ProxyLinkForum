c85929ca90eac8533c54b2bf63a6a0f49f850555
Xon <tazipper1@hotmail.com>
2015-07-29 14:41:07 +0800

    Whitespace fixup

--- upload/library/SV/ProxyLinkForum/XenForo/ControllerPublic/LinkForum.php
+++ upload/library/SV/ProxyLinkForum/XenForo/ControllerPublic/LinkForum.php
@@ -1,15 +1,15 @@
 <?php
 class SV_ProxyLinkForum_XenForo_ControllerPublic_LinkForum extends XFCP_SV_ProxyLinkForum_XenForo_ControllerPublic_LinkForum
 {
-	protected function _getLinkOrError($linkId)
-	{        
-		$link = parent::_getLinkOrError($linkId);
+    protected function _getLinkOrError($linkId)
+    {        
+        $link = parent::_getLinkOrError($linkId);
         
         if (isset($link['sv_is_proxy']) && is_numeric($link['link_url']))
         {
             $link['link_url'] = XenForo_Link::buildPublicLink('forums', array('node_id' => $link['link_url']));
         }
 
-		return $link;
-	}
+        return $link;
+    }
 }
