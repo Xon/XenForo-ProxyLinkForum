610cef52bebe10b41648fdee2c92efccddcd61ad
Xon <tazipper1@hotmail.com>
2015-07-30 10:08:04 +0800

    Fix typo which broke non-proxied link forums.

--- upload/library/SV/ProxyLinkForum/XenForo/ControllerPublic/LinkForum.php
+++ upload/library/SV/ProxyLinkForum/XenForo/ControllerPublic/LinkForum.php
@@ -5,7 +5,7 @@ class SV_ProxyLinkForum_XenForo_ControllerPublic_LinkForum extends XFCP_SV_Proxy
     {
         $link = parent::_getLinkOrError($linkId);
 
-        if (!!empty($link['sv_proxy_node_id']) && is_numeric($link['sv_proxy_node_id']))
+        if (!empty($link['sv_proxy_node_id']) && is_numeric($link['sv_proxy_node_id']))
         {
             $link['link_url'] = XenForo_Link::buildPublicLink('forums', array('node_id' => $link['sv_proxy_node_id']));
         }
