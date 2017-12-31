361c3ad9ad774271944b7f67e5e090eaa8cdf82b
Xon <tazipper1@hotmail.com>
2018-01-01 06:14:18 +0800

    Enforce minimum XF & php version

--- upload/library/SV/ProxyLinkForum/Installer.php
+++ upload/library/SV/ProxyLinkForum/Installer.php
@@ -5,6 +5,19 @@ class SV_ProxyLinkForum_Installer
     public static function install($existingAddOn, $addOnData)
     {
         $version = isset($existingAddOn['version_id']) ? $existingAddOn['version_id'] : 0;
+        $required = '5.4.0';
+        $phpversion = phpversion();
+        if (version_compare($phpversion, $required, '<'))
+        {
+            throw new XenForo_Exception(
+                "PHP {$required} or newer is required. {$phpversion} does not meet this requirement. Please ask your host to upgrade PHP",
+                true
+            );
+        }
+        if (XenForo_Application::$versionId < 1030070)
+        {
+            throw new Exception('XenForo 1.3.0+ is Required!');
+        }
 
         SV_Utils_Install::addColumn("xf_link_forum", "sv_proxy_node_id", "INT NOT NULL DEFAULT 0");
 
